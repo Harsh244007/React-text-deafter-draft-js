bba4461b616e7e374bacfe58335a1205be103fff
Harsh Patel <Patelharsh241999@gmail.com>
2024-02-05 10:58:04 +0000

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ const App = () => {
             calendarProps={{
               header(args) {
                 console.log(args, "calendar props");
-                return <></>;
+                return <button>Header</button>;
               },
               children: (e) => {
                 const selectedDate = new Date(e.date).getDate();
