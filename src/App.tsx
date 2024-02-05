c30e1f59b11d57c582a340f1a77ff60a2a8b446e
Harsh Patel <Patelharsh241999@gmail.com>
2024-02-05 10:04:32 +0000

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,82 @@
-import { Suspense, useContext } from "react";
-import { ThemeContext } from "./Contexts/ThemeContext";
-import { Header, Draft, Loading } from "./Components";
+import { deepMerge } from "grommet/utils";
+import React from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Grid,
+  grommet,
+  DateInput,
+  Grommet,
+  Heading,
+  Page,
+  PageContent,
+  PageHeader,
+  Paragraph,
+  ResponsiveContext,
+  Text,
+} from "grommet";
 
 const App = () => {
-  const { theme } = useContext(ThemeContext);
+  const theme = deepMerge(grommet, {
+    global: {
+      colors: {
+        brand: "#228BE6",
+      },
+      font: {
+        family: "Roboto",
+        size: "14px",
+        height: "20px",
+      },
+    },
+  });
+
+  const [value, setValue] = React.useState(new Date().toISOString());
+  const onChange = (event) => {
+    const nextValue = event.value;
+    setValue(nextValue);
+  };
+  const apiDates = [4, 7, 13, 29];
   return (
-    <main className={theme}>
-      <section className="max-w-screen-xl w-full m-auto p-2">
-        <Suspense fallback={<Loading />}>
-          <Header />
-        </Suspense>
-        <Suspense fallback={<Loading />}>
-          <Draft />
-        </Suspense>
-      </section>
-    </main>
+    <Grommet theme={theme} full themeMode={"dark"}>
+      <main>
+        <Box align="center" pad="large">
+          <DateInput
+            format="mm/dd/yyyy"
+            size="medium"
+            style={{ height: "80px", width: "200px" }}
+            value={value}
+            calendarProps={{
+              children: (e) => {
+                const selectedDate = new Date(e.date).getDate();
+                if (apiDates.includes(selectedDate)) {
+                  return (
+                    <button
+                      key={selectedDate}
+                      className="selectedDate date w-8 h-8 bg-red-600"
+                    >
+                      {selectedDate}
+                    </button>
+                  );
+                }
+                return (
+                  <button
+                    key={selectedDate}
+                    className="relative date w-8 h-8 bg-blue-400"
+                  >
+                    {selectedDate}
+                  </button>
+                );
+              },
+            }}
+            onChange={onChange}
+          />
+        </Box>
+      </main>
+    </Grommet>
   );
 };
 
